4eadee4b08e4d33b596f46a0d2f9056b90935028
Daniel <89273037+7645re@users.noreply.github.com>
2024-04-16 21:44:51 +0300

    init

---
+++ DiagnosticContext/DiagnosticContext.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Prometheus;
+
+namespace DiagnosticContext;
+
+public class DiagnosticContext
+{
+    private readonly Counter _metricCounterDuration = Metrics.CreateCounter(
+        "method_execution_duration",
+        "Counts the duration of method executions in milliseconds",
+        new CounterConfiguration
+        {
+            LabelNames = new[]
+            {
+                "method_name",
+                "entry_point"
+            },
+        }
+    );
+
+    private readonly Counter _metricCounterExecution = Metrics.CreateCounter(
+        "method_execution_count",
+        "Counts the number of method executions",
+        new CounterConfiguration
+        {
+            LabelNames = new[]
+            {
+                "method_name",
+                "entry_point"
+            }
+        }
+    );
+
+    private readonly string _entryPoint;
+
+    public DiagnosticContext(string entryPoint)
+    {
+        _entryPoint = entryPoint;
+    }
+
+    public IDisposable Measure([CallerMemberName] string methodName = "")
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        return new DisposableAction(() =>
+        {
+            stopwatch.Stop();
+            _metricCounterDuration.WithLabels(methodName, _entryPoint).Inc(stopwatch.ElapsedMilliseconds);
+            _metricCounterExecution.WithLabels(methodName, _entryPoint).Inc();
+        });
+    }
+
+    private class DisposableAction : IDisposable
+    {
+        private readonly Action _action;
+
+        public DisposableAction(Action action)
+        {
+            _action = action;
+        }
+
+        public void Dispose()
+        {
+            _action.Invoke();
+        }
+    }
+}
